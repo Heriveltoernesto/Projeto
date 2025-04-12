cf0c74206ab53ee8e9c913d7d77ed4603d8a24cc
Heriveltoernesto <heriveltoernesto@outlook.com>
2025-04-12 18:40:39 -0300

    FirebaseConfg.js

---
+++ FirebaseConfg.js
@@ -0,0 +1,17 @@
+// src/firebaseConfig.js
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "SUA_API_KEY",
+  authDomain: "SEU_DOMINIO",
+  projectId: "SEU_PROJECT_ID",
+  storageBucket: "SEU_BUCKET",
+  messagingSenderId: "SEU_SENDER_ID",
+  appId: "SEU_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
